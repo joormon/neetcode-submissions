d93b4f19fda206e307fb9b9be917aea62c4f14d4
Akash Deep Gogoi <169077958+joormon@users.noreply.github.com>
2026-08-16 21:15:22 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,52 @@
+class Solution {
+   public:
+    string minWindow(string s, string t) {
+        if (!s.length()  || !t.length()) {
+            return "";
+        }
+        unordered_map<char, int> tmap;
+        unordered_map<char, int> windowMap;
+        int minLength = INT_MAX;
+        int startIdx = 0;
+
+        for (char e : t) {
+            tmap[e]++;
+        }
+
+        int required = t.size();
+        int formed = 0;
+
+        int left = 0, right = 0;
+
+        while (right < s.length()) {
+            char c = s[right];
+            if (tmap.count(c)) {
+                windowMap[c]++;
+                if (tmap[c] >= windowMap[c]) {
+                    formed++;
+                }
+            }
+
+            while (formed == required) {
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    startIdx = left;
+                }
+
+                char x = s[left];
+                if (tmap.count(x)) {
+                    windowMap[x]--;
+                    if (windowMap[x] < tmap[x]) {
+                        formed--;
+                    }
+                   
+                }
+                 left++;
+                
+            }
+            right++;
+        }
+
+        return minLength==INT_MAX ? "":s.substr(startIdx, minLength);
+    }
+};
