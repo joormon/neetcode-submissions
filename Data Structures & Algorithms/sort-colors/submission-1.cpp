84d37d440d2d775857762f6e20f8d9b07cb34a92
Akash Deep Gogoi <169077958+joormon@users.noreply.github.com>
2026-09-11 13:37:45 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    //performed Counting Sort;
+    void sortColors(vector<int>& nums) 
+    {
+        vector<int> cntArray(4,0);
+
+        for(int ele:nums)
+        {
+            cntArray[ele]++;
+        }
+        
+        //creating prefix sum to maintain the order
+        for(int i=1;i<4;i++)
+        {
+            cntArray[i]=cntArray[i]+cntArray[i-1];
+        }
+
+        int n=nums.size();
+        vector<int> ans(n);
+
+        for(int i=n-1;i>=0;i--)
+        {
+            ans[cntArray[nums[i]]-1]=nums[i];
+            cntArray[nums[i]]--;
+        }
+
+        nums=ans;
+    }
+};
