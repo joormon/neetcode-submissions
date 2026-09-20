ed8cd1412fc60cdbb81f44cea9681bea4714537f
Akash Deep Gogoi <169077958+joormon@users.noreply.github.com>
2026-09-20 12:22:47 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) 
+    {
+        int l=1;
+        int r=x;
+        int ans=0;
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            //To tackle overflow:
+            //option1: use long long 
+            //option2: m*m<=x -> m<=x/m;
+            if(m <= x/m)
+            {
+                ans=m;
+                l=m+1;
+            }
+            else
+            {
+                r=m-1;
+            }
+        } 
+
+        return ans;   
+    }
+};
