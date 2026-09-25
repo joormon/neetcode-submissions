3b0fdde27b28852d930ddb1646e03f30c674ee29
Akash Deep Gogoi <169077958+joormon@users.noreply.github.com>
2026-09-25 13:23:09 +0530

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.cpp
@@ -0,0 +1,56 @@
+class MyQueue {
+    stack<int> s1,s2;
+public:
+    MyQueue() {
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        if(s1.empty()) return -1;
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        int pop_element=s2.top();
+        s2.pop();
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+
+        return pop_element;
+    }
+    
+    int peek() {
+        if(s1.empty()) return -1;
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        int top_element=s2.top();
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+
+        return top_element;
+    }
+    
+    bool empty() {
+        return s1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
